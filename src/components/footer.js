059a68945cb08e02d73cf86fbecf99fe31678398
Tom Wright <tomnwright@outlook.com>
2021-04-27 19:04:58 +0100

    Fixed repo link

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            Created using React by Tom Wright &copy; 2021. <a href="http://www.github.com/tomnwright/react-snake-game">Github Repo</a>
+            Created using React by Tom Wright &copy; 2021. <a href="http://www.github.com/tomnwright/React-Snake">Github Repo</a>
         </footer>
     )
 }
