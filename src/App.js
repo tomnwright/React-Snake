cd9129c8bd4bcfe218b2a50e996640063fbd2f63
Tom Wright <tomnwright@outlook.com>
2021-04-27 18:49:52 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Board from "./components/board"
+import Footer from "./components/footer"
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Board />
+      <p>Use the arrow keys to move the snake.</p>
     </div>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            Created using React by Tom Wright &copy; 2021. <a href="http://www.github.com/tomnwright/react-snake-game">Github Repo</a>
+        </footer>
+    )
+}
+
+export default Footer
